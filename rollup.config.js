8526ce0081f9c3a2a09a3c00bf01e67286c5c982
hota1024 <hotalog@hota1024.com>
2021-09-14 05:58:58 +0900

    🎉  =^.  .^= < init!!!

--- rollup.config.js
+++ rollup.config.js
@@ -1,55 +1,54 @@
 import ts from '@wessberg/rollup-plugin-ts'
 import pkg from './package.json'
 import eslint from '@rollup/plugin-eslint'
+import html from 'rollup-plugin-html'
 import { builtinModules } from 'module'
+
+const external = [
+  ...builtinModules,
+  ...(pkg.dependencies == null ? [] : Object.keys(pkg.dependencies)),
+  ...(pkg.devDependencies == null ? [] : Object.keys(pkg.devDependencies)),
+  ...(pkg.peerDependencies == null ? [] : Object.keys(pkg.peerDependencies)),
+]
 
 export default [
   {
-    input: 'src/index.ts',
+    input: 'src/twitter/twitter.ts',
     output: [
       {
-        file: pkg.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: pkg.module,
-        format: 'esm',
+        file: 'dist/twitter/twitter.js',
+        format: 'iife',
+        name: 'Yanderetter',
         sourcemap: true,
       },
     ],
     plugins: [eslint(), ts()],
-    external: [
-      ...builtinModules,
-      ...(pkg.dependencies == null ? [] : Object.keys(pkg.dependencies)),
-      ...(pkg.devDependencies == null ? [] : Object.keys(pkg.devDependencies)),
-      ...(pkg.peerDependencies == null
-        ? []
-        : Object.keys(pkg.peerDependencies)),
+    external,
+  },
+  {
+    input: 'src/instagram/instagram.ts',
+    output: [
+      {
+        file: 'dist/instagram/yanderegram.js',
+        format: 'iife',
+        name: 'Yanderegram',
+        sourcemap: true,
+      },
     ],
+    plugins: [eslint(), ts()],
+    external,
   },
   {
     input: 'src/ui/ui.ts',
     output: [
       {
-        file: 'dist/cjs/ui.js',
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: 'dist/esm/ui.js',
-        format: 'esm',
+        file: 'dist/ui/ui.js',
+        format: 'iife',
+        name: 'YandereUI',
         sourcemap: true,
       },
     ],
-    plugins: [eslint(), ts()],
-    external: [
-      ...builtinModules,
-      ...(pkg.dependencies == null ? [] : Object.keys(pkg.dependencies)),
-      ...(pkg.devDependencies == null ? [] : Object.keys(pkg.devDependencies)),
-      ...(pkg.peerDependencies == null
-        ? []
-        : Object.keys(pkg.peerDependencies)),
-    ],
+    plugins: [eslint(), ts(), html({ include: 'src/ui/**/*.html' })],
+    external,
   },
 ]
--- src/getProgress.ts
+++
@@ -1,6 +0,0 @@
-export async function getProgress(): Promise<number> {
-  const res = await fetch('http://localhost:8080/task/progress')
-  const { progress } = await res.json()
-
-  return progress
-}
