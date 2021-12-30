16842e12aab85570b6ce31529a7bfe071c2d5e3d
= <hotalog@hota1024.com>
2021-12-30 23:54:13 +0900

    ✨add issueAuthURL

--- src/core/api/API.ts
+++ src/core/api/API.ts
@@ -15,9 +15,18 @@ export class API {
    * fetch progress.
    */
   async getProgress(): Promise<number> {
-    const { progress } = await this.get<{ progress: number }>('/task/progress')
+    const { progress } = await this.get<{ progress: number }>('task/progress')
 
     return progress
+  }
+
+  /**
+   * issue auth url.
+   */
+  async issueAuthURL(): Promise<string> {
+    const { url } = await this.get<{ url: string }>('googletask/request')
+
+    return url
   }
 
   /**
@@ -34,6 +43,31 @@ export class API {
   }
 
   /**
+   * post request.
+   *
+   * @param url url.
+   * @param data post data.
+   */
+  async post<T>(url: string, data?: unknown): Promise<T> {
+    const apiURL = await this.buildURL(url)
+
+    const req = data
+      ? fetch(apiURL, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+      : fetch(apiURL, {
+          method: 'POST',
+        })
+    const res = await req
+
+    return res.json()
+  }
+
+  /**
    * build url with api endpoint url.
    *
    * @param url url.
@@ -41,6 +75,8 @@ export class API {
   async buildURL(url: string): Promise<string> {
     const { apiEndpoint } = await this.config.get()
 
-    return apiEndpoint + url
+    const slash = apiEndpoint.endsWith('/') ? '' : '/'
+
+    return apiEndpoint + slash + url
   }
 }
