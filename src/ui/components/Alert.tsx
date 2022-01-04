4fd0a58856a6fadcfa540268e924f93d4322adfd
= <hotalog@hota1024.com>
2022-01-04 22:35:46 +0900

    💄add Alert  component

---
+++ src/ui/components/Alert.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+/**
+ * Alert props.
+ */
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type AlertProps = {}
+
+/**
+ * Alert component.
+ */
+export const Alert = styled.div<AlertProps>`
+  padding: 1rem;
+  border-left: 4px solid #42a5f5;
+`
