502ef43b1adb2c4b1bea8e6f2805c525e150fab3
= <hotalog@hota1024.com>
2022-01-05 19:40:32 +0900

    ✨add debug view

--- src/ui/Main.tsx
+++ src/ui/Main.tsx
@@ -1,5 +1,6 @@
+import { api } from '@/core/api'
 import { config } from '@/core/config'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card } from './components/Card'
 import { Configuration } from './components/Configuration'
 import { Container } from './components/Container'
@@ -10,13 +11,29 @@ import { PageTitle } from './components/PageTitle'
  * Main component.
  */
 export const Main: React.VFC = () => {
+  const [progress, setProgress] = useState<number>()
+
+  const updateProgress = async () => {
+    const p = await api.getProgress()
+    setProgress(p)
+  }
+
+  useEffect(() => {
+    updateProgress()
+  }, [])
+
   return (
     <>
       <Container>
         <PageTitle>せってい</PageTitle>
         <Content>
           <Card>
+            <h2>きほんせってい</h2>
             <Configuration config={config} />
+          </Card>
+          <Card>
+            <h2>デバッグ</h2>
+            {typeof progress != 'undefined' && <p>進捗率: {progress}</p>}
           </Card>
         </Content>
       </Container>
