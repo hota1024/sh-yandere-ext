7173d2dce62461cf31977779fb8538d6c2fdfa61
= <hotalog@hota1024.com>
2022-01-13 16:08:59 +0900

    ✨add message tokens

--- src/core/messages.ts
+++ src/core/messages.ts
@@ -1,3 +1,6 @@
+import { config } from './config'
+import { decodeJWT } from './decodeJWT'
+
 type Message = {
   rangeStart: number
   rangeEnd: number
@@ -8,7 +11,7 @@ export const messages: Message[] = [
   {
     rangeStart: 0,
     rangeEnd: 0.25,
-    message: '{name}どうして逃げるの？',
+    message: '{name}くん、どうして逃げるの？',
   },
   {
     rangeStart: 0.25,
@@ -31,4 +34,22 @@ export const getMessagesInRange = (p: number): Message[] => {
   return messages.filter((message) => {
     return message.rangeStart <= p && p < message.rangeEnd
   })
+}
+
+export const replaceMessageTokens = async (
+  message: string
+): Promise<string> => {
+  const { jwt } = await config.get()
+  const payload = decodeJWT(jwt)
+
+  if (!payload) {
+    return 'あれ？まだログインしてないの？ログイン方法調べてほしいなぁ...'
+  }
+
+  const { fullname, surname, name } = payload.user
+
+  return message
+    .replace(/\{fullname\}/g, fullname)
+    .replace(/\{surname\}/g, surname)
+    .replace(/\{name\}/g, name)
 }
